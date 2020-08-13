08a200ecb6e3d48c83c02fcc518db315b3aea273
Гордиенко Савелий Андреевич <GSA4@azot.kmr>
2020-08-13 09:48:47 +0700

    create reacr-app

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <header>
+            <h1>Кошелек</h1>
+            <h2>Калькулятор расходов</h2>
+        </header>
+    )
+}
